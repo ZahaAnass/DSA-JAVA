324b3a1229ca2452154a756b3319d4b1b62e4622
ZahaAnass <zahaanass277@gmail.com>
2025-07-12 00:02:53 +0100

    Add example usage of DynamicArray implementation

--- 05-Dynamic-Arays/Main.java
+++ 05-Dynamic-Arays/Main.java
@@ -6,15 +6,21 @@ public class Main{
 
         // ArrayList<String> arrayList = new ArrayList<String>();
 
-        DynamicArray dynamicArray = new DynamicArray();
+        DynamicArray dynamicArray = new DynamicArray(5);
 
         dynamicArray.add("A");
         dynamicArray.add("B");
         dynamicArray.add("C");
+        dynamicArray.add("D");
+        dynamicArray.add("E");
+        dynamicArray.add("F"); // This will trigger a grow operation
     
         dynamicArray.insert(0, "X"); // Insert X at index 0
 
         dynamicArray.delete("A"); // Delete element B
+
+        int index = dynamicArray.search("C"); // Search for element C
+        System.out.println("Index of C: " + index); // Should print the index of C
 
         System.out.println("Dynamic Array created with default capacity: " + dynamicArray.capacity);
         System.out.println("Array is empty: " + dynamicArray.isEmpty());
