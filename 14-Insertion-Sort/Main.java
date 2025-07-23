ba01f658027e4c2de8231dd9b3f9a0704ddd7f2f
ZahaAnass <zahaanass277@gmail.com>
2025-07-23 21:24:34 +0100

    Insertion Sort

--- 14-Insertion-Sort/Main.java
+++ 14-Insertion-Sort/Main.java
@@ -3,10 +3,15 @@ public class Main {
     public static void main(String[] args) {
         
 
-        // insertion sort = take an element and insert it into the correct position in the sorted part of the array.
+        // insertion sort = after comparing elements to the left
+        //                  shift elements to the right to make room to insert a value
+
         //                  Time Complexity: Quadratic Time O(n^2)
-        //                  small data set = okey-ish
+        //                  small data set = decent
         //                  large data set = BAD
+
+        //                  Less steps that Bubble Sort
+        //                  Best case is O(n) compared to Selection Sert O(n^2)
 
         int[] array = {9, 1, 8, 2, 7, 3, 6, 4, 5};
 
@@ -16,25 +21,23 @@ public class Main {
             System.out.print(i + " ");
         }
 
+        System.out.println("");
 
     }
 
     private static void insertionSort(int[] array) {
-        // Start with the second element (i=1) assuming first element is already sorted
-        for (int i = 1; i < array.length; i++) {
-            // Store current element to be inserted
-            int current = array[i];
+        
+        for(int i = 0; i < array.length; i++){
+            int temp = array[i];
             int j = i - 1;
-            
-            // Compare current with each element on the left until a smaller element is found
-            // or beginning of array is reached
-            while (j >= 0 && array[j] > current) {
-                array[j + 1] = array[j]; // Shift elements to make space
+
+            while(j >= 0 && array[j] > temp){
+                array[j + 1] = array[j];
                 j--;
             }
-            
-            // Insert current element at correct position
-            array[j + 1] = current;
+
+            array[j + 1] = temp;
         }
+        
     }
 }
