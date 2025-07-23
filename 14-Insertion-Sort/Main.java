5c63fec0b82ef3059b308030efcd0d415594b708
ZahaAnass <zahaanass277@gmail.com>
2025-07-23 21:27:16 +0100

    Insertion Sort

--- 14-Insertion-Sort/Main.java
+++ 14-Insertion-Sort/Main.java
@@ -9,7 +9,7 @@ public class Main {
         //                  Time Complexity: Quadratic Time O(n^2)
         //                  small data set = decent
         //                  large data set = BAD
-
+    
         //                  Less steps that Bubble Sort
         //                  Best case is O(n) compared to Selection Sert O(n^2)
 
@@ -26,18 +26,21 @@ public class Main {
     }
 
     private static void insertionSort(int[] array) {
-        
-        for(int i = 0; i < array.length; i++){
-            int temp = array[i];
+        // Start with the second element (i=1) assuming first element is already sorted
+        for (int i = 1; i < array.length; i++) {
+            // Store current element to be inserted
+            int current = array[i];
             int j = i - 1;
-
-            while(j >= 0 && array[j] > temp){
-                array[j + 1] = array[j];
+            
+            // Compare current with each element on the left until a smaller element is found
+            // or beginning of array is reached
+            while (j >= 0 && array[j] > current) {
+                array[j + 1] = array[j]; // Shift elements to make space
                 j--;
             }
-
-            array[j + 1] = temp;
+            
+            // Insert current element at correct position
+            array[j + 1] = current;
         }
-        
     }
 }
