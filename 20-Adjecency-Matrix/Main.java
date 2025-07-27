73d9048a4890ec847bb6d86b1534338c09265bc9
ZahaAnass <zahaanass277@gmail.com>
2025-07-27 14:12:58 +0100

    Adjencecy Matrix

--- 20-Adjecency-Matrix/Main.java
+++ 20-Adjecency-Matrix/Main.java
@@ -25,8 +25,19 @@ public class Main {
         graph.addEdge(4, 0); // E <-> A
         graph.addEdge(4, 2); // E <-> C
 
+        graph.print();
+
+        System.out.println("Edge A <-> B: " + graph.checkEdge(0, 1));
+        System.out.println("Edge B <-> D: " + graph.checkEdge(1, 3));
+        System.out.println("Edge C <-> E: " + graph.checkEdge(2, 4));
+        System.out.println("Edge E <-> A: " + graph.checkEdge(4, 0));
+        System.out.println("Edge D <-> A: " + graph.checkEdge(3, 0));
+        System.out.println("Edge E <-> C: " + graph.checkEdge(4, 2));
+        System.out.println("Edge B <-> C: " + graph.checkEdge(1, 2));
+        System.out.println("Edge A <-> D: " + graph.checkEdge(0, 3));
+        System.out.println("Edge D <-> E: " + graph.checkEdge(3, 4));
+        System.out.println("Edge C <-> D: " + graph.checkEdge(2, 3));
 
     }
-    
 
 }
